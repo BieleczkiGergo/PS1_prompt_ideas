7250f01ffb4ea6b5cbe26a2179d5e41f355d0976
Tipikaly <bgergo8b@gmail.com>
2024-07-02 11:31:12 +0200

    responsive multiline prompt

--- main.sh
+++ main.sh
@@ -38,6 +38,19 @@ echo -e $CONSTRUCTION_START" construction ██\e[0m"
 
 echo ""
 
-echo -e "\e[0m ~ | show git branch \e[31m$(git branch --show-current) \e[0m>"
+echo -e "\e[0m ~ | show git branch \e[31m( $(git branch --show-current) ) \e[0m>"
 # I'm sure some people didn't know they can do this. Especially noobs like myself.
+
+echo ""
+
+responsive_start="│ $PWD ├"
+responsive_line_len=$(($(tput cols) - ${#responsive_start} - 1))
+responsive_line="─"
+for i in $(seq 1 $responsive_line_len)
+do
+	responsive_line="$responsive_line─"
+done
+responsive="$responsive_start$responsive_line└"
+echo -e "\e[0m\e[32m$responsive \e[0mresponsive multiline \e[32m>\e[0m"
+
 echo ""
